7ec148d76b86fcadeeed70481eed15d6b4643f91
Jitanshu <jitanshupatra06@gmail.com>
2025-07-23 21:41:48 +0530

    Initial commit

--- server.js
+++ server.js
@@ -186,6 +186,7 @@ app.put('/edit-product/:id', checkAdmin, async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
